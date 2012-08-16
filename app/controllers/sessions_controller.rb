5d34c24a016e071197fbb5ee012d18baaf3d375d
NatashaMurashev <nmurashev@gmail.com>
2012-08-16 13:51:43 -0700

    redirects to user path on sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     TwitterClient.get_tweets(user)
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to user_path(user.twitter_name), :notice => "Signed in!"
   end
 
   def destroy
